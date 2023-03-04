24a8f3af1e53ee935b3ef4776340854cae21cff4
robchendev <robchendev@gmail.com>
2023-03-03 22:21:49 -0800

    fixed override font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    // TODO: Set theme
+    fontFamily: {
+      sans: ["PT Serif", "serif"],
+    },
     colors: {
       transparent: "transparent",
       current: "currentColor",
