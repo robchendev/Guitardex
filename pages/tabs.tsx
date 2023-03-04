c766b975c31512ef85df5748b0d83312e74f6d6e
robchendev <robchendev@gmail.com>
2023-03-03 19:48:04 -0800

    HStack ordered tab item preview

--- pages/tabs.tsx
+++ pages/tabs.tsx
@@ -1,26 +1,49 @@
-import { Center, Input } from "@chakra-ui/react";
+import { Center, HStack, Input } from "@chakra-ui/react";
 import { NextPage } from "next";
 import React from "react";
+import { FaSpotify, FaYoutube } from "react-icons/fa";
 import Wrapper from "../components/Wrapper";
 import { TabInfo, tabs } from "../config/tabs";
 
 const TabItem = ({ tab }: { tab: TabInfo }) => {
   return (
     <div>
-      <div>{tab.title}</div>
-      <div>{tab.source}</div>
-      <div>{tab.artist}</div>
-      <div>{tab.genre}</div>
-      <div>{tab.tuning.name}</div>
-      <div>{tab.tuning.strings.toString()}</div>
-      <div>{tab.videoLink}</div>
-      <div>{tab.spotifyLink}</div>
-      {tab.button.type === "buy" && (
+      <HStack>
         <div>
-          {tab.button.price}, {tab.button.link}
+          <div>{tab.title}</div>
+          <div>{tab.source}</div>
+          <div>{tab.artist}</div>
         </div>
-      )}
-      {tab.button.type === "free" && <div>free, {tab.button.link}</div>}
+        <div>
+          {tab.button.type === "buy" && (
+            <div>
+              {tab.button.price} <a href={tab.button.link}>Buy</a>
+            </div>
+          )}
+          {tab.button.type === "free" && (
+            <div>
+              free, <a href={tab.button.link}>Get</a>
+            </div>
+          )}
+        </div>
+        <div>{tab.genre}</div>
+        <div>
+          <div>{tab.tuning.name}</div>
+          <div>{tab.tuning.strings.toString()}</div>
+        </div>
+        <div>
+          {tab.videoLink && (
+            <a href={tab.videoLink}>
+              <FaYoutube />
+            </a>
+          )}
+          {tab.spotifyLink && (
+            <a href={tab.videoLink}>
+              <FaSpotify />
+            </a>
+          )}
+        </div>
+      </HStack>
     </div>
   );
 };
