1b74f28e97d280bd2fec617fdb38b9d5b84a9e4f
dead-weight <nibiruassault@gmail.com>
2023-03-05 11:08:53 +0200

    added constantine

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ export const Index: NextPage = () => {
               Tommy Emmanuel to start learning fingerstyle. When he was 14, he
               was given the honor of being able to perform with Tommy Tommy on
               stage, which was uploaded as his first video on YouTube. A year
-              later, he decided to create his own voice on the guitar, shifting
+              later, he decided to create his own voice on the guigtar, shifting
               his focus to arranging and covering songs. In 2013, he began
               taking YouTube more seriously and started building an audience.
             </p>
