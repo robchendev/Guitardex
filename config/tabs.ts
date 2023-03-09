df24c3c75dd5171924a6b0609e36dca0d203b991
robchendev <robchendev@gmail.com>
2023-03-08 23:17:14 -0800

    Tabs accordion detailed view and stable design

--- config/tabs.ts
+++ config/tabs.ts
@@ -35,6 +35,8 @@ export type TabInfo = {
   videoLink?: string;
   spotifyLink?: string;
   button?: Buy | Free;
+  guitardex?: string;
+  difficulty: number;
 };
 
 export const tabs: TabInfo[] = [
@@ -53,14 +55,16 @@ export const tabs: TabInfo[] = [
       price: 7.99,
       link: "https://www.musicnotes.com/sheetmusic/mtd.asp?ppn=MN0268543",
     },
+    guitardex: "https://gdex.cc/?Suzume_14.5.17.1.2.4.16.6.7.10.11.12.22",
+    difficulty: 5,
   },
   {
-    title: "Suzume",
-    source: "Suzume no Tojimari OST",
+    title: "Something really long",
+    source: "A really long subtitle that overflows to ellipsesspo",
     artist: "RADWIMPS",
-    videoLink: "https://youtu.be/wQANVR4vqXI",
-    spotifyLink: "https://youtu.be/wQANVR4vqXI",
-    genre: "Anime",
+    videoLink: "https://www.youtube.com/watch?v=wQANVR4vqXI",
+    spotifyLink: "https://open.spotify.com/track/3CqV5uSdPvj5Ux3E8Idusc?si=6365db22464049ef",
+    genre: "Alternative",
     tuning: {
       name: "Open D#maj7",
       strings: ["D#", "G", "D", "G", "A#", "D"],
@@ -70,5 +74,6 @@ export const tabs: TabInfo[] = [
       price: 7.99,
       link: "https://www.musicnotes.com/sheetmusic/mtd.asp?ppn=MN0268543",
     },
+    difficulty: 7,
   },
 ];
--- pages/tabs.tsx
+++ pages/tabs.tsx
@@ -7,6 +7,8 @@ import {
   Box,
   Center,
   HStack,
+  IconButton,
+  Link,
   Text,
   VStack,
 } from "@chakra-ui/react";
@@ -15,108 +17,235 @@ import React from "react";
 import { FaSpotify, FaYoutube } from "react-icons/fa";
 import Wrapper from "../components/Wrapper";
 import { Buy, Free, TabInfo, tabs } from "../config/tabs";
+import { ImStarEmpty, ImStarHalf, ImStarFull } from "react-icons/im";
+
+const purgeLink = (link: string) => link.replace(/http(s)?(:)?(\/\/)?|(\/\/)?(www\.)?/g, "");
 
 const TabButton = ({ button }: { button: Buy | Free }) => {
   switch (button.type) {
     case "buy":
       return (
-        <div>
-          ${button.price} <a href={button.link}>Buy</a>
-        </div>
+        <HStack>
+          <Text>${button.price}</Text>
+          <a
+            href={button.link}
+            className="block ml-2 px-3 pb-2 pt-1.5 rounded-md bg-carmine-soft hover:bg-carmine-hard transition ease-in duration-300"
+          >
+            Buy
+          </a>
+        </HStack>
       );
     case "free":
       return (
-        <div>
-          free, <a href={button.link}>Get</a>
-        </div>
+        <HStack>
+          <Text>free</Text>
+          <a
+            href={button.link}
+            className="block 1ml-2 px-3 pb-2 pt-1.5 rounded-md bg-carmine-soft hover:bg-carmine-hard transition ease-in duration-300"
+          >
+            Get
+          </a>
+        </HStack>
       );
   }
 };
 
+const TabDetailPrice = ({ button }: { button: Buy | Free }) => {
+  switch (button.type) {
+    case "buy":
+      return <Text>${button.price}</Text>;
+    case "free":
+      return <Text>free</Text>;
+  }
+};
+
+const TabDetailLink = ({ button }: { button: Buy | Free }) => {
+  switch (button.type) {
+    case "buy":
+      return (
+        <Text noOfLines={1}>
+          <a className="text-gold" href={button.link}>
+            {purgeLink(button.link)}
+          </a>
+        </Text>
+      );
+    case "free":
+      return (
+        <Text noOfLines={1}>
+          <a className="text-gold" href={button.link}>
+            placeholder link for free
+          </a>
+        </Text>
+      );
+  }
+};
+
+const Difficulty = ({ rating }: { rating: number }) => {
+  const full = Math.floor(rating / 2);
+  const half = rating % 2;
+  const empty = Math.floor((10 - rating) / 2);
+  console.log([...Array(full)]);
+  return (
+    <HStack spacing={1}>
+      {[...Array(full)].map((val: number, index: number) => (
+        <ImStarFull key={index} />
+      ))}
+      {half === 1 && <ImStarHalf />}
+      {[...Array(empty)].map((val: number, index: number) => (
+        <ImStarEmpty key={index} />
+      ))}
+    </HStack>
+  );
+};
+
 const TabItem = ({ tab }: { tab: TabInfo }) => {
   return (
-    <AccordionItem className="py-4 w-full px-0 border-none">
+    <AccordionItem className="py-4 w-full px-0 border-none box shadow-md shadow-black-hard/20">
       <AccordionButton px={0} className="w-full" justifyContent="space-between" textAlign="left">
-        {/* <AccordionIcon /> */}
-        <Box>
-          <Text noOfLines={1} className="text-gold">
+        <Box w="45%">
+          <Text fontWeight={500} noOfLines={1} className="text-gold">
             {tab.title}
           </Text>
           <Text noOfLines={1}>{tab.source}</Text>
-          {/* <Text noOfLines={1}>{tab.artist}</Text> */}
         </Box>
-        <HStack justifyContent="space-between" spacing={8}>
+        <HStack justifyContent="space-between" spacing={7}>
           <div>
-            <Text>{tab.tuning.name}</Text>
             <Text>{tab.tuning.strings.join(" ")}</Text>
           </div>
-          <Center>{tab.genre}</Center>
+          <Center w={20}>
+            <Text>{tab.genre}</Text>
+          </Center>
           <Center>{tab.button && <TabButton button={tab.button} />}</Center>
-          <VStack>
-            {tab.videoLink && (
-              <a href={tab.videoLink}>
-                <FaYoutube size={24} />
-              </a>
-            )}
-            {tab.spotifyLink && (
-              <a href={tab.videoLink}>
-                <FaSpotify size={24} />
-              </a>
-            )}
-          </VStack>
+          <HStack>
+            <Link href={tab.spotifyLink} isExternal>
+              <IconButton
+                onClick={(e) => e.stopPropagation()}
+                as={FaSpotify}
+                aria-label="Spotify"
+                bgColor="transparent"
+                _hover={{ bgColor: "transparent" }}
+                className={
+                  tab.spotifyLink
+                    ? "text-white-soft hover:text-gold transition ease-in duration-300"
+                    : "text-grey-med"
+                }
+                size="sm"
+                disabled={!tab.spotifyLink}
+              />
+            </Link>
+            <Link href={tab.videoLink} isExternal>
+              <IconButton
+                onClick={(e) => e.stopPropagation()}
+                as={FaYoutube}
+                aria-label="Youtube"
+                bgColor="transparent"
+                _hover={{ bgColor: "transparent" }}
+                className={
+                  tab.videoLink
+                    ? "text-white-soft hover:text-gold transition ease-in duration-300"
+                    : "text-grey-med"
+                }
+                size="sm"
+                disabled={!tab.videoLink}
+              />
+            </Link>
+          </HStack>
         </HStack>
       </AccordionButton>
-      <AccordionPanel pb={4}>
-        {tab.source && <div>From: {tab.source}</div>}
-        <div>Artist: {tab.artist}</div>
+      <AccordionPanel p={0} pt={2} pb={3}>
+        <table className="[&>tbody>tr>td:first-child]:w-0 [&>tbody>tr]:border-grey-med [&>tbody>tr]:border-t [&>tbody>tr]:border-b  [&>tbody>tr>td]:py-1 [&>tbody>tr>td:first-child]:text-white-ghost [&>tbody>tr>td:first-child]:pr-4 w-full">
+          <tbody>
+            <tr>
+              <td>Link</td>
+              <td>
+                <TabDetailLink button={tab.button} />
+              </td>
+            </tr>
+            <tr>
+              <td>Difficulty</td>
+              <td>
+                <Difficulty rating={tab.difficulty} />
+              </td>
+            </tr>
+            {tab.guitardex && (
+              <tr>
+                <td>Guitardex</td>
+                <td>
+                  <a className="text-gold" href={tab.guitardex}>
+                    <Text noOfLines={1}>{purgeLink(tab.guitardex)}</Text>
+                  </a>
+                </td>
+              </tr>
+            )}
+            <tr>
+              <td>Song</td>
+              <td>{tab.title}</td>
+            </tr>
+            <tr>
+              <td>Source</td>
+              <td>{tab.source}</td>
+            </tr>
+            <tr>
+              <td>Artist(s)</td>
+              <td>{tab.artist}</td>
+            </tr>
+            <tr>
+              <td>Price</td>
+              <td>
+                <TabDetailPrice button={tab.button} />
+              </td>
+            </tr>
+            <tr>
+              <td>Genre</td>
+              <td>{tab.genre}</td>
+            </tr>
+            <tr>
+              <td>Tuning</td>
+              <td>{tab.tuning.name}</td>
+            </tr>
+            <tr>
+              <td>Strings</td>
+              <td>{tab.tuning.strings.join(" ")}</td>
+            </tr>
+            {tab.videoLink && (
+              <tr>
+                <td>Youtube</td>
+                <td>
+                  <a className="text-gold" href={tab.videoLink}>
+                    <Text noOfLines={1}>{purgeLink(tab.videoLink)}</Text>
+                  </a>
+                </td>
+              </tr>
+            )}
+            {tab.spotifyLink && (
+              <tr>
+                <td>Spotify</td>
+                <td>
+                  <a className="text-gold" href={tab.spotifyLink}>
+                    <Text noOfLines={1}>{purgeLink(tab.spotifyLink)}</Text>
+                  </a>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
       </AccordionPanel>
     </AccordionItem>
-    // <div className="py-4 w-full">
-    //   <HStack justifyContent="space-between">
-    //     <Box>
-    //       <Text noOfLines={1} className="text-gold">
-    //         {tab.title}
-    //       </Text>
-    //       <Text noOfLines={1}>{tab.source}</Text>
-    //       {/* <Text noOfLines={1}>{tab.artist}</Text> */}
-    //     </Box>
-    //     <HStack justifyContent="space-between" spacing={8}>
-    //       <div>
-    //         <Text>{tab.tuning.name}</Text>
-    //         <Text>{tab.tuning.strings.join(" ")}</Text>
-    //       </div>
-    //       <Center>{tab.genre}</Center>
-    //       <Center>{tab.button && <TabButton button={tab.button} />}</Center>
-    //       <VStack>
-    //         {tab.videoLink && (
-    //           <a href={tab.videoLink}>
-    //             <FaYoutube size={24} />
-    //           </a>
-    //         )}
-    //         {tab.spotifyLink && (
-    //           <a href={tab.videoLink}>
-    //             <FaSpotify size={24} />
-    //           </a>
-    //         )}
-    //       </VStack>
-    //     </HStack>
-    //   </HStack>
-    // </div>
   );
 };
 
 const Tabs: NextPage = () => {
   return (
-    <Wrapper title="tabs">
+    <Wrapper title="Tabs">
       <Center mb={4}>
         <input
-          placeholder="search song, tuning, genre"
+          placeholder="Search song, tuning, genre"
           className="border-gold border-px rounded-md py-3 px-4 bg-grey-hard w-72"
         />
       </Center>
       <Accordion allowToggle w="full">
         <Center>
-          <VStack w="80%" divider={<div className="h-px w-full bg-grey-med " />}>
+          <VStack w="100%" spacing={4}>
             {tabs.map((tab: TabInfo, index: number) => (
               <TabItem key={index} tab={tab} />
             ))}
