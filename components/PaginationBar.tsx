83d897e48c8e027638f7cb158ce4a1e3c576e023
robchendev <robchendev@gmail.com>
2023-03-21 14:33:27 -0700

    pagination styling

--- components/PaginationBar.tsx
+++ components/PaginationBar.tsx
@@ -1,14 +1,36 @@
-import {
-  Flex,
-  HStack,
-  Input,
-  NumberDecrementStepper,
-  NumberIncrementStepper,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-} from "@chakra-ui/react";
+import { Box, Flex, HStack, Input, Select } from "@chakra-ui/react";
 import React from "react";
+import {
+  AiOutlineLeft,
+  AiOutlineRight,
+  AiOutlineVerticalLeft,
+  AiOutlineVerticalRight,
+} from "react-icons/ai";
+
+const PButton = ({
+  onClick,
+  children,
+  disabled = false,
+}: {
+  onClick: () => void;
+  children: React.ReactNode;
+  disabled?: boolean;
+}) => {
+  return (
+    <button
+      className={
+        "h-10 w-10 rounded-md flex justify-center items-center text-2xl " +
+        (disabled
+          ? "bg-grey-med"
+          : "bg-carmine-soft hover:bg-carmine-hard transition ease-in duration-250")
+      }
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
 
 const PaginationBar = ({
   pagination,
@@ -24,20 +46,27 @@ const PaginationBar = ({
   mb?: number;
 }) => {
   return (
-    <HStack
-      mt={mt}
-      mb={mb}
-      className="w-full justify-center [&>button]:bg-grey-med [&>button]:py-1 [&>button]:px-3"
-    >
-      <button onClick={() => onChange(0)}>{"|<"}</button>
-      <button onClick={() => onChange(pagination - 1)}>{"<"}</button>
-      <Flex>
-        Page
-        <Input width="2em" p="1" h="2em" value={pagination + 1} />
-        of {maxPage}
-      </Flex>
-      <button onClick={() => onChange(pagination + 1)}>{">"}</button>
-      <button onClick={() => onChange(maxPage - 1)}>{">|"}</button>
+    <HStack mt={mt} mb={mb} className="w-full justify-center">
+      <HStack>
+        <PButton onClick={() => onChange(0)} disabled={pagination <= 0}>
+          <AiOutlineVerticalRight />
+        </PButton>
+        <PButton onClick={() => onChange(pagination - 1)} disabled={pagination <= 0}>
+          <AiOutlineLeft />
+        </PButton>
+      </HStack>
+      <Box w="4em" textAlign="center" px={2} className="text-xl font-medium">
+        {pagination + 1} / {maxPage}
+        {/* <Input mx="5px" width="1.75em" p="1" h="1.75em" textAlign="center" value={pagination + 1} /> */}
+      </Box>
+      <HStack>
+        <PButton onClick={() => onChange(pagination + 1)} disabled={pagination + 1 >= maxPage}>
+          <AiOutlineRight />
+        </PButton>
+        <PButton onClick={() => onChange(maxPage - 1)} disabled={pagination + 1 >= maxPage}>
+          <AiOutlineVerticalLeft />
+        </PButton>
+      </HStack>
     </HStack>
   );
 };
--- components/TabList.tsx
+++ components/TabList.tsx
@@ -8,7 +8,7 @@ import {
   SlideFade,
   useDisclosure,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { TabInfo } from "../types/tabs";
 import PaginationBar from "./PaginationBar";
 import TabItem from "./TabItem";
@@ -37,7 +37,7 @@ const TabList = ({
   const showSearchResults = !(!search && min === 0 && max === 10);
   const noMatch = showSearchResults && !searchResults.length;
   const isDifficultyFilter = !(min === 0 && max === 10);
-  const { isOpen, onToggle } = useDisclosure();
+  const tabsPerPage = 10;
   return (
     <>
       <Accordion
@@ -58,7 +58,7 @@ const TabList = ({
           {showSearchResults ? (
             <>
               {searchResults
-                .slice(10 * pagination, 10 * (pagination + 1))
+                .slice(tabsPerPage * pagination, tabsPerPage * (pagination + 1))
                 .map((matchingIndex: number) => (
                   <TabItem
                     key={matchingIndex}
@@ -70,7 +70,7 @@ const TabList = ({
           ) : (
             <>
               {tabs
-                .slice(10 * pagination, 10 * (pagination + 1))
+                .slice(tabsPerPage * pagination, tabsPerPage * (pagination + 1))
                 .map((tab: TabInfo, index: number) => (
                   <TabItem key={index} tab={tab} isDifficultyFilter={isDifficultyFilter} />
                 ))}
@@ -81,13 +81,12 @@ const TabList = ({
       {/* Idea: let user set pagination to 5, 10, 15, 20 and store pref in localstorage */}
       {/* Idea: show search bar on bottom of page on the same fixed block as pagination
           --> initially a button that slides open a search input when clicked */}
-      <div className="h-20"></div>
       <div className="fixed bottom-0 left-0 right-0 bg-grey-ghost h-20 flex items-center">
         <div className="lg:max-w-4xl lg:mx-auto w-full">
           <PaginationBar
             pagination={pagination}
             onChange={(page: number) => setPagination(page)}
-            maxPage={Math.ceil(tabs.length / 10)}
+            maxPage={Math.ceil(tabs.length / tabsPerPage)}
           />
         </div>
       </div>
