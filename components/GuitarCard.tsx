1b3252b225c2677285b2683c7ee11f3cb0a92248
dead-weight <nibiruassault@gmail.com>
2023-03-21 21:51:46 +0200

    Resolved issues mentioned in code review

--- components/GuitarCard.tsx
+++ components/GuitarCard.tsx
@@ -36,15 +36,15 @@ const BuyButton = ({ item }: { item: ItemComponent }) => {
     case "buy":
       return (
         <div className="flex align-middle mt-4">
-          <span className="w-max text-4xl px-6">${item.price}</span>
-          <a href={item.buyLink} className="bg-carmine-soft w-max text-lg px-5 py-2 rounded-sm">
+          <span className="text-4xl px-6">${item.price}</span>
+          <a href={item.buyLink} className="bg-carmine-soft text-lg px-5 py-2 rounded-sm">
             Buy this guitar
           </a>
         </div>
       );
     case "discontinued":
       return (
-        <a href={item.buyLink} className="bg-grey-soft w-max text-2xl p-2 rounded-md mt-4">
+        <a href={item.buyLink} className="bg-grey-soft text-2xl p-2 rounded-md mt-4">
           Discontinued
         </a>
       );
--- components/HeaderMobile.tsx
+++ components/HeaderMobile.tsx
@@ -7,6 +7,7 @@ import {
   DrawerBody,
   DrawerContent,
   Icon,
+  DrawerOverlay,
 } from "@chakra-ui/react";
 import { useDisclosure } from "@chakra-ui/react";
 import { useRouter } from "next/router";
@@ -21,38 +22,36 @@ const HeaderMobile = () => {
   return (
     <div className="sm:block lg:hidden ">
       <header className=" flex text-white-soft p-4 px-10 mx-auto md:w-full max-w-7xl justify-between items-center text-center">
-        <Image
-          src="public/img/evdm.png"
-          fallbackSrc="https://eddievdmeer.com/wp-content/uploads/elementor/thumbs/cropped-evdmlogo-oqa4dzno6q4ah9xd5u0hy03itfxfgiimm19em295vk.png"
-          alt="evdm-logo"
-        />
+        <Image src="public/img/evdm.png" alt="evdm-logo" />
         <Button ref={btnRef} colorScheme="" onClick={onOpen}>
           <Icon as={FaAlignJustify} w={10} h={10} />
         </Button>
         <Drawer isOpen={isOpen} placement="top" onClose={onClose} finalFocusRef={btnRef}>
-          <DrawerContent className="bg-transparent top-30">
-            <DrawerBody className="bg-black-soft text-white-soft">
-              <VStack>
-                {navItems.map((navItem: NavItem, index: number) => (
-                  <Link
-                    key={index}
-                    href={navItem.link}
-                    className={`font-serif  text-2xl px-5 py-3.5 hover:text-gold${
-                      router.pathname === navItem.link ? " text-purple-soft" : ""
-                    }`}
+          <DrawerOverlay className="top-20">
+            <DrawerContent className="bg-transparent top-30">
+              <DrawerBody className="bg-black-soft text-white-soft">
+                <VStack>
+                  {navItems.map((navItem: NavItem, index: number) => (
+                    <Link
+                      key={index}
+                      href={navItem.link}
+                      className={`font-serif  text-2xl px-5 py-3.5 hover:text-gold w-full text-center${
+                        router.pathname === navItem.link ? " text-purple-soft" : ""
+                      }`}
+                    >
+                      {navItem.name}
+                    </Link>
+                  ))}
+                  <button
+                    onClick={onClose}
+                    className="font-serif text-2xl pl-10 px-5 py-3.5 hover:text-gold"
                   >
-                    {navItem.name}
-                  </Link>
-                ))}
-                <button
-                  onClick={onClose}
-                  className="font-serif text-2xl pl-10 px-5 py-3.5 hover:text-gold"
-                >
-                  cancel <Icon as={FaChevronUp}></Icon>
-                </button>
-              </VStack>
-            </DrawerBody>
-          </DrawerContent>
+                    cancel <Icon as={FaChevronUp}></Icon>
+                  </button>
+                </VStack>
+              </DrawerBody>
+            </DrawerContent>
+          </DrawerOverlay>
         </Drawer>
       </header>
     </div>
