158456103d21c857c07e1ab4cd7001d4d8a46adf
robchendev <robchendev@gmail.com>
2023-09-03 18:35:25 -0700

    removed height html on global css,should fix production mode scroll issue

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -1,5 +1,5 @@
 import { Flex } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect } from "react";
 import PageTitle from "./PageTitle";
 import Sidebar from "./Sidebar";
 
