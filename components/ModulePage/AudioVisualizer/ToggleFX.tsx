346c4e2d52c2b68db6bf5b1c0322176bb4b5b755
robchendev <robchendev@gmail.com>
2023-09-12 00:25:35 -0700

    fixed wrong boolean logic in FX button show variation

--- components/ModulePage/AudioVisualizer/ToggleFX.tsx
+++ components/ModulePage/AudioVisualizer/ToggleFX.tsx
@@ -6,7 +6,7 @@ const ToggleFX = ({ isOn, onClick }: { isOn: boolean; onClick: () => void }) => 
   return (
     <button
       className={`px-3 py-2 h-10 rounded-md font-medium ${
-        isOn ? "border-grey bg-grey text-ghost" : "border-purple bg-purple text-white"
+        isOn ? "border-purple bg-purple text-white" : "border-grey bg-grey text-ghost"
       }`}
       onClick={onClick}
     >
