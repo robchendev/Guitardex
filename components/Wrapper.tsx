6af7d2c48fa7efa69be1cfd0a0e0b57a268168d9
robchendev <robchendev@gmail.com>
2023-12-02 01:03:24 -0800

    Fixed mobile navigation and media queried body overflow behavior

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -2,7 +2,7 @@ import React, { Suspense, lazy } from "react";
 import PageTitle, { PageTitleMobile } from "./PageTitle";
 import Sidebar from "./Sidebar";
 import { FiAlignJustify } from "react-icons/fi";
-import { Icon, useDisclosure, Flex } from "@chakra-ui/react";
+import { Icon, useDisclosure, Flex, HStack } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
 const WrapperDrawer = dynamic(() => import("./WrapperDrawer"), {
   loading: () => <p>Loading...</p>,
@@ -12,25 +12,25 @@ const Wrapper = ({ children, title }: { children: React.ReactNode; title?: strin
   const { isOpen, onOpen, onClose } = useDisclosure();
   const btnRef = React.useRef(null);
   return (
-    <div className="h-full bg-bg2 lg:py-16 lg:pb-16 lg:hp-24 font-fredoka font-normal tracking-[0.4px]">
-      {/* <div className="flex justify-end w-full h-16 lg:!hidden lg:h-0 lg:w-0 fixed top-0"> */}
-      <div className="w-[10px] h-[10px] m-4" />
-      <PageTitleMobile title={title ?? "GuitarDex"} />
-      <button ref={btnRef} className="w-10 h-10 m-4 " onClick={onOpen}>
-        <Icon w={10} h={10} as={FiAlignJustify} />
-      </button>
-      {/* <PageTitle title={title ?? "Guitardex"}></PageTitle> */}
-      {/* </div> */}
-
+    <div className="h-full bg-bg2 lg:py-16 lg:pb-16 lg:hp-24 font-fredoka font-normal tracking-[0.4px] ">
+      <HStack
+        justifyContent="space-between"
+        className="bg-bg2 fixed w-full z-[999] border-b border-text lg:relative lg:!hidden"
+      >
+        <div className="w-12 h-12" />
+        <PageTitleMobile title={title ?? "GuitarDex"} />
+        <button ref={btnRef} className="w-12 h-12 m-4 " onClick={onOpen}>
+          <Icon w={8} h={8} as={FiAlignJustify} />
+        </button>
+      </HStack>
       <WrapperDrawer isOpen={isOpen} btnRef={btnRef} onClose={onClose} />
-
-      <div className="h-full max-w-[1100px] m-auto my-0 px-8">
+      <div className="h-full max-w-[1100px] m-auto px-8">
         <Flex className="justify-between">
           <div className="hidden lg:block lg:w-1/4 ">
             <Sidebar />
           </div>
           <div className="w-full lg:w-3/4">
-            <div className="h-full lg:ml-8 ">
+            <div className="h-full lg:ml-8 mt-16 lg:mt-0">
               <div className="hidden lg:block ">
                 {title && <PageTitle title={title ?? "Guitardex"} />}
               </div>
